476c2758f2dae515b2ab51e9669b9a4405620b14
DineshSisodiya <35620721+DineshSisodiya@users.noreply.github.com>
2018-01-20 13:33:39 +0530

    Update main.js

--- fusionCharts/myjs/main.js
+++ fusionCharts/myjs/main.js
@@ -1,4 +1,10 @@
 
+/*
+    - Define the data array , using this chart will be drawn
+    - Data is required
+    - For customize specific anchor properties describe in the data array related to that point
+    - To draw vertical line on any value add required key-values in data array.
+*/
 var data  = [
                 {
                    /* 
